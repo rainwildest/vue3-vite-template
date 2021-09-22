1fbc035ca8d3475577677691b45dda03d5ffc518
DESKTOP-HQAQVNT\Administrator <rainwildest@163.com>
2021-09-22 11:35:34 +0800

    update vite.config add svgicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import viteSvgIcons from "vite-plugin-svg-icons";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx({
-    // options are passed on to @vue/babel-plugin-jsx
-  })]
-})
+  plugins: [
+    vue(),
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+    }),
+    viteSvgIcons({
+      iconDirs: [path.resolve(process.cwd(), "src/assets/svg")],
+      symbolId: "icon-[dir]-[name]"
+    })
+  ]
+});
